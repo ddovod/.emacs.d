a43bb559b3b7a96f34df4584c45dd83369577c33
ddovod <ddovod@gmail.com>
2019-06-05 11:09:36 +0300

    speeding up svn

--- user_elisp/projectile_config.el
+++ user_elisp/projectile_config.el
@@ -12,6 +12,7 @@
 (define-key projectile-mode-map (kbd "C-c p r") 'projectile-run-project)
 (setq projectile-project-compilation-cmd "")
 (setq projectile-project-run-cmd "")
+(setq projectile-svn-command "find . -type f -not -iwholename '*.svn/*' -print0")
 
 (provide 'projectile_config)
 ;;; projectile_config.el ends here
